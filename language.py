4ad36db3e3d666a300cd5e8ec6d73d831c48b873
David Shams <david.shams@gmail.com>
2018-06-27 11:34:42 -0500

    None is false

--- language.py
+++ language.py
@@ -2,6 +2,21 @@
 def add_nums(a,b):
     return a + b
 
+# Strings
+print("""Documentation""");
+print("capitalize".capitalize());
+print("hello".replace("e", "a"));
+print("hello".isalpha());
+print("123".isdigit()); #Check before converting
+print("some, cvs, values".split(","));
+name = "David"
+message = "Hello"
+print("{1} {0}".format(name, message));
+true = True
+false = False
+print(int(true))
+print(int(false))
+
 # Sets
 
 # Tuples
@@ -9,3 +24,14 @@ def add_nums(a,b):
 # Frozen Sets
 
 print(add_nums(2,3));
+
+# Truthy Falsy
+number = 5
+if number:
+    print("Number {0} is defined".format(number))
+
+no = None
+if no:
+    print("no is not false")
+else:
+    print("no is false")
